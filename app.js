e581f9cba631c5e02f1f7f0a8e47502a91184092
leeseungjin <dltmdwls154@gmail.com>
2021-08-14 00:06:53 +0900

    color 선택 구현

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
-ctx.strokeStyle = '#2c2c2c'
-ctx.lineWidth = 2.5;
+const rgba = document.getElementById('jsRange');
+const colors = document.getElementsByClassName('jsColor');
+
+canvas.width = 700;
+canvas.height = 700;
+ctx.strokeStyle = '#2c2c2c';
+
+ctx.lineWidth = rgba.value;
+const linewidth = () => {
+  ctx.lineWidth = rgba.value;
+}
+
+
 let painting = false;
 
 
@@ -15,18 +26,23 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if(!painting){
+    // Path 는 선을 생성하는 함수
     ctx.beginPath();
+    // X, Y 에 선을 그려주는 함수
     ctx.moveTo(x,y);
+  }else {
+    ctx.lineTo(x,y);
+    ctx.stroke();
   }
 }
-function onMouseDown(event) {
-  painting = true;
+const handleColorClick = (event) => {
+  ctx.strokeStyle = event.target.style.backgroundColor;
 }
-
-
 if(canvas){
   canvas.addEventListener('mousemove', onMouseMove);
   canvas.addEventListener('mousedown', startPainting);
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
+  rgba.addEventListener('mouseup', linewidth)
 }
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick))
